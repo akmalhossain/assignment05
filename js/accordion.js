76680a5aae71fa2b93173fbcb5028d58f083cf2d
akmalhossain <akmalforu@gmail.com>
2024-09-24 13:54:13 +0600

    accordion updated

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,33 @@
 
 const questions = document.querySelectorAll('.questions');
 for (singleQ of questions){
-    singleQ.addEventListener('click', function(){ //1 when click on a single question 
-        // 
-        const answer = document.querySelectorAll('.answer');
-        for(let singleAns of answer){
-            if( singleAns !== this.nextElementSibling){  //2 when click if open paragraph is no a sibling 
-                singleAns.classList.remove('open') //2 solution then close it
+    singleQ.addEventListener('click', function(){ 
+        const answers = document.querySelectorAll('.answer');
+        const arrowUpDown = document.querySelectorAll('.rotate-180');
+        for(let singleAns of answers){
+            if( singleAns !== this.nextElementSibling){  
+                singleAns.classList.remove('open') 
+            } 
+        }
+        for (let singleArrow of arrowUpDown){
+            if(singleArrow !== this.querySelector('.rotate-180')){
+                singleArrow.classList.remove('rotate-180');
+                singleArrow.classList.add('rotate-0');
             }
         }
-        // 
-        // const openAccordion = this.nextElementSibling;
-        this.nextElementSibling.classList.toggle('open'); //1 solution : open next sibling means paragraph
+        const openAccordion = this.nextElementSibling;
+        openAccordion.classList.toggle('open'); 
+        const arrowNow = this.querySelector('.rotate-0') 
+        || this.querySelector('.rotate-180');
+        if(arrowNow){
+            if(!openAccordion.classList.contains('open')){
+                arrowNow.classList.add('rotate-0');
+                arrowNow.classList.remove('rotate-180');
+            }
+            else{
+            arrowNow.classList.remove('rotate-0');
+            arrowNow.classList.add('rotate-180');
+        }
+        }
     })
 }
